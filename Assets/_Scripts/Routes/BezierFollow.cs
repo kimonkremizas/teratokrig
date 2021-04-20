27536af6e856fbff13e865cff60154f46d17e98d
Nicolai <nico388a@edu.easj.dk>
2021-04-20 17:05:21 +0200

    Boss Route

---
+++ Assets/_Scripts/Routes/BezierFollow.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] routes;
+
+    private int routeToGo;
+
+    private float tParam;
+
+    private Vector2 bossPosition;
+
+    private float speedModifier;
+
+    private bool coroutineAllowed;
+ 
+    void Start()
+    {
+        routeToGo = 0;
+        tParam = 0f;
+        speedModifier = 0.5f;
+        coroutineAllowed = true;
+    }
+
+    void Update()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine(GoByTheRoute(routeToGo));
+        }   
+    }
+
+    private IEnumerator GoByTheRoute(int routeNumber)
+    {
+        coroutineAllowed = false;
+        Vector2 p0 = routes[routeNumber].GetChild(0).position;
+        Vector2 p1 = routes[routeNumber].GetChild(1).position;
+        Vector2 p2 = routes[routeNumber].GetChild(2).position;
+        Vector2 p3 = routes[routeNumber].GetChild(3).position;
+
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speedModifier;
+            bossPosition = Mathf.Pow(1 - tParam, 3) * p0 +
+                3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 +
+                3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 +
+                Mathf.Pow(tParam, 3) * p3;
+
+            transform.position = bossPosition;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        tParam = 0f;
+        routeToGo += 1;
+
+        if (routeToGo > routes.Length - 1)
+            routeToGo = 0;
+
+        coroutineAllowed = true;
+    }
+
+
+}
---
+++ Assets/_Scripts/Routes/BossRoute.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRoute : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] controlPoints;
+
+    private Vector2 gizmosPosition;
+
+    private void OnDrawGizmos()
+    {
+        for (float t = 0; t <= 1; t += 0.05f)
+        {
+            gizmosPosition = Mathf.Pow(1 - t, 3) * controlPoints[0].position +
+                3 * Mathf.Pow(1 - t, 2) * t * controlPoints[1].position +
+                3 * (1 - t) * Mathf.Pow(t, 2) * controlPoints[2].position +
+                Mathf.Pow(t, 3) * controlPoints[3].position;
+
+            Gizmos.DrawSphere(gizmosPosition, 0.25f);
+        }
+
+        Gizmos.DrawLine(new Vector2(controlPoints[0].position.x, controlPoints[0].position.y),
+            new Vector2(controlPoints[1].position.x, controlPoints[1].position.y));
+
+        Gizmos.DrawLine(new Vector2(controlPoints[2].position.x, controlPoints[2].position.y),
+            new Vector2(controlPoints[3].position.x, controlPoints[3].position.y));
+    }
+}
